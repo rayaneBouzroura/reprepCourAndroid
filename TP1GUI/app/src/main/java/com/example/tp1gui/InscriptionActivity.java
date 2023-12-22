27b72cc57ed56fdebe9cb54c7c9e36753eab1fed
Rayane <2111752@cegepmontpetit.ca>
2023-12-22 13:22:40 -0600

    TP1 stopped at accueuil activity
    reste :
    drawer implementation
    task creation
    some btns intent thingies
    AGAHTGHHHH

---
+++ TP1GUI/app/src/main/java/com/example/tp1gui/InscriptionActivity.java
@@ -0,0 +1,14 @@
+package com.example.tp1gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+    //sign up activity (no drawer)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
