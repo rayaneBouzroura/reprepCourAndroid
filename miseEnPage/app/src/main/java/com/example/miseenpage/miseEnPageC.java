9ddc2dcd61f8df10f2a02f5a3b1febaeb8abd7f9
Rayane <2111752@cegepmontpetit.ca>
2024-01-22 18:06:42 -0600

    ajout exercice semaine 1 mise en page

---
+++ miseEnPage/app/src/main/java/com/example/miseenpage/miseEnPageC.java
@@ -0,0 +1,14 @@
+package com.example.miseenpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class miseEnPageC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mise_en_page_c);
+    }
+}
