345d4f47d050cc9f658a07db2cdcbb6488a76d1c
Rayane <2111752@cegepmontpetit.ca>
2024-01-26 16:11:08 -0600

    TP1GUI update
    ajout d'une abstract activity qui gere la logique du drawer layout et la tester en l'implementer dans une test activity
    for now le toggle marche faudra implementer le routing
    idee : abstract class dans abstract activity qui gere le routing
    chaque activite fils l'ecrira pour definir un routing specifique

---
+++ TP1GUI/app/src/main/java/com/example/tp1gui/BaseActivity.java
@@ -0,0 +1,58 @@
+package com.example.tp1gui.services;
+
+import android.content.Context;
+import android.renderscript.ScriptGroup;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.tp1gui.R;
+import com.google.android.material.navigation.NavigationView;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    //stuff we want to implement in order to make other activities
+    //that inherit this one more consistent and easy to write without repeating code:
+
+    //activite accueuil / consultations / creation :
+    // - gestion drawer bar
+    // - gestion bouton drawer bar selon le contexte
+
+    //activite accueuil seul activite avec le FAB pour creer une activite
+
+    //la methode gestionDrawerBar necessite :
+    // - un drawerLayout
+    // - un actionBarDrawerToggle (init l'objet necessite le contexte de l'appli "this" ainsi
+
+
+    //abstract methods qui retourne le nav view et le drawer layout
+    protected abstract NavigationView getNavView();
+    protected abstract DrawerLayout getDrawerLayout();
+
+    //abstract method qui gere le nav item bouton de la drawer bar selon le contexte (va etre implementer dans chaque activite)
+    protected abstract void gestionBoutonDrawerBar(@NonNull MenuItem item);
+    public void gestionDrawerBar() {
+
+        NavigationView  nv = getNavView();
+        DrawerLayout drawerLayout = getDrawerLayout();
+        //gestion drawer bar
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this ,
+                drawerLayout,
+                R.string.nav_open,
+                R.string.nav_close);
+        //syncstate
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                gestionBoutonDrawerBar(item);
+                return true;
+            }
+        });
+    }
+}
---
+++ TP1GUI/app/src/main/java/com/example/tp1gui/testActivity.java
@@ -0,0 +1,16 @@
+package com.example.tp1gui.services;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tp1gui.R;
+
+public class testActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
